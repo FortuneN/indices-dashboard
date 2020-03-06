53f86a6a4b827148b3a4fb88add9eac11df043de
FortuneN <fortunengwenya@gmail.com>
2020-03-07 00:17:57 +0200

    ...

--- setup.sh
+++ setup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-sudo apt-get update && apt-get upgrade -y
-sudo apt-get install chromium x11-xserver-utils unclutter
+sudo apt-get purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 -y
+sudo apt-get purge smartsim java-common minecraft-pi libreoffice* -y
+
+sudo apt-get clean
+sudo apt-get autoremove -y
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install xdotool unclutter chromium-browser sed
+
+#echo "
+#@xset s off
+#@xset -dpms
+#@xset s noblank
+#@sed -i 's/\"exited_cleanly\": false/\"exited_cleanly\": true/' ~/.config/chromium-browser Default/Preferences
+#@chromium-browser --noerrdialogs --kiosk https://blockdev.io --incognito --disable-translate
+#" > /etc/xdg/lxsession/LXDE/autostart
+#~/.config/lxsession/LXDE-pi/autostart
