e836a2c14efa0d7b323ccde2b60ab10a185dce48
FortuneN <fortunengwenya@gmail.com>
2020-03-07 23:52:10 +0200

    ...

--- install-indices-dashboard.sh
+++ install-indices-dashboard.sh
@@ -9,7 +9,7 @@
 chmod +x *.sh
 mkdir -p /usr/share/indices-dashboard
 cp -r -f ./** /usr/share/indices-dashboard
-ln -s -f /etc/init.d/start-indices-dashboard.sh /usr/share/indices-dashboard/start-indices-dashboard.sh
+ln -s -f /usr/share/indices-dashboard/start-indices-dashboard.sh /etc/init.d/start-indices-dashboard.sh
 
 # restart
 
