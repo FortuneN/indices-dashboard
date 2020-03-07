8bf2f272eacf4e5c6d16e851d5cafd8eb01e3ea5
FortuneN <fortunengwenya@gmail.com>
2020-03-07 22:18:10 +0200

    ...

--- setup-kiosk.sh
+++ setup-kiosk.sh
@@ -2,16 +2,16 @@
 
 # copy files to 'deployment' location
 
-sudo mkdir /usr/share/dashboard
-sudo cp -r ./** /usr/share/dashboard
+mkdir /usr/share/dashboard
+cp -r ./** /usr/share/dashboard
 
 # setup kiosk mode
 
-sudo echo "
-/usr/share/dashboard/setup.sh
+echo "
+/usr/share/dashboard/start.sh
 /usr/bin/chromium --kiosk --ignore-certificate-errors --disable-restore-session-state \"https://www.google.com\"
 " > /etc/xdg/lxsession/LXDE-pi/autostart
 
 # restart computer
 
-sudo reboot
+reboot
