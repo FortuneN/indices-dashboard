748b296bdd2c480e9e4880d84369de4ba8bbfd66
FortuneN <fortunengwenya@gmail.com>
2020-03-08 00:46:26 +0200

    ...

--- install-indices-dashboard.sh
+++ install-indices-dashboard.sh
@@ -6,9 +6,8 @@
 
 # install
 
-apt-get install unclutter -y
-
 chmod +x *.sh
+apt-get install unclutter -y
 mkdir -p /usr/share/indices-dashboard
 cp -r -f ./** /usr/share/indices-dashboard
 ln -f /usr/share/indices-dashboard/start-indices-dashboard.sh /etc/init.d/start-indices-dashboard.sh
