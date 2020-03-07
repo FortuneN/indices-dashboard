45ebbec31b9374695f775d644a355df949c387bf
FortuneN <fortunengwenya@gmail.com>
2020-03-08 01:36:24 +0200

    ...

--- start-indices-dashboard.sh
+++ start-indices-dashboard.sh
@@ -13,13 +13,14 @@ xset -dpms
 
 # hide mouse pointer
 
-unclutter -idle 0.5 -root &
+#unclutter -idle 0.5 -root &
 
-# make sure chrome never has catastrophic-exit state
+# make sure chromium is never in a catastrophic-exit state
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
 # start chromium
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8000
+#/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8000
+/usr/bin/chromium-browser http://localhost:8000
