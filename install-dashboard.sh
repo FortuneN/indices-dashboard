4c2fc31bac2578dd6bf7890b4e3aae24f1cc64d3
FortuneN <fortunengwenya@gmail.com>
2020-03-08 02:25:41 +0200

    ...

--- install-dashboard.sh
+++ install-dashboard.sh
@@ -4,10 +4,20 @@
 
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
-# install
+# ensure all files are readable
 
-chmod +x *.sh
+chmod -R +r **
+
+# ensure all scripts are executable
+
+chmod -R +x *.sh
+
+# install unclutter
+
 apt-get install unclutter -y
+
+# create and start service
+
 cp -f indices-dashboard.service /lib/systemd/system
 systemctl enable indices-dashboard.service
 systemctl start  indices-dashboard.service
--- start-dashboard.sh
+++ start-dashboard.sh
@@ -2,7 +2,7 @@
 
 # start web app
 
-nohup python -m SimpleHTTPServer &
+python -m SimpleHTTPServer &
 
 # disable blank screen
 
@@ -12,14 +12,13 @@ xset -dpms
 
 # hide mouse pointer
 
-#unclutter -idle 0.5 -root &
+unclutter -idle 0.5 -root &
 
-# make sure chromium is never in a catastrophic-exit state
+# make sure chromium is never in a "catastrophic exit" state
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/'   /home/pi/.config/chromium/Default/Preferences
 
 # start chromium
 
-#/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8000
-/usr/bin/chromium-browser http://localhost:8000
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8000
