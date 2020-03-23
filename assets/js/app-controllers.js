c0a8a31d1624c87bd719133948bb3e921badde99
Fortune Ngwenya <fortunengwenya@gmail.com>
2020-03-23 15:27:20 +0200

    Update app-controllers.js

--- assets/js/app-controllers.js
+++ assets/js/app-controllers.js
@@ -4,16 +4,7 @@ app.controller('appController', ['$scope', '$timeout', '$mdDialog', 'dataSource'
     $scope.data2 = [];
     $scope.offline = true; // always start offline
     
-    dataSource.onData(function (data) {
-        $scope.data = data;
-        $scope.offline = false;
-    });
-
-    dataSource.onOffline(function () {
-        $scope.offline = true;
-    });
-    
-    dataSource.getData().then(function (data) {
+    function refreshData (data) {
         
         $scope.data1 = [];
         $scope.data2 = [];
@@ -27,6 +18,17 @@ app.controller('appController', ['$scope', '$timeout', '$mdDialog', 'dataSource'
                 $scope.data2.push(item);
             }
         });
+    }
+    
+    dataSource.onData(function (data) {
+        $scope.offline = false;
+        refreshData(data);
     });
+
+    dataSource.onOffline(function () {
+        $scope.offline = true;
+    });
+    
+    dataSource.getData().then(refreshData);
 
 }]);
